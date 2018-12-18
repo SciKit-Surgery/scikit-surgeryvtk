b21bc2f2d5d0ea64b48d10e2cd0843556d29928f
Matt Clarkson <m.clarkson@ucl.ac.uk>
2018-12-18 12:05:24 +0000

    Issue #11: Force offscreen rendering.

--- tests/vtk/conftest.py
+++ tests/vtk/conftest.py
@@ -3,6 +3,7 @@
 import pytest
 from PySide2.QtWidgets import QApplication
 import numpy as np
+import vtk
 from sksurgeryoverlay.vtk.vtk_overlay_window import VTKOverlayWindow
 
 
@@ -19,7 +20,11 @@ def setup_qt():
 def vtk_overlay(setup_qt):
     """ Creates a VTKOverlayWindow with blank background image. """
     image = np.ones((150, 100, 3), dtype=np.uint8)
+    factory = vtk.vtkGraphicsFactory()
+    factory.SetOffScreenOnlyMode(1)
+    factory.SetUseMesaClasses(1)
     vtk_overlay = VTKOverlayWindow()
+    vtk_overlay.GetRenderWindow().SetOffScreenRendering(1)
     vtk_overlay.set_video_image(image)
     return vtk_overlay
 
@@ -37,6 +42,10 @@ def vtk_overlay_with_gradient_image(setup_qt):
             image[y][x][0] = y
             image[y][x][1] = y
             image[y][x][2] = y
+    factory = vtk.vtkGraphicsFactory()
+    factory.SetOffScreenOnlyMode(1)
+    factory.SetUseMesaClasses(1)
     vtk_overlay = VTKOverlayWindow()
+    vtk_overlay.GetRenderWindow().SetOffScreenRendering(1)
     vtk_overlay.set_video_image(image)
     return setup_qt, vtk_overlay, image
