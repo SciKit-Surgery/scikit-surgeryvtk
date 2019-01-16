7896f0e4ab1bfc35d40b204afdddd3de725a3832
Matt Clarkson <m.clarkson@ucl.ac.uk>
2019-01-16 17:01:43 +0000

    Issue #12: Create functions and some tests to set extrinsics and intrinsics on vtkCamera.

--- sksurgeryvtk/camera/vtk_camera_model.py
+++ sksurgeryvtk/camera/vtk_camera_model.py
@@ -1,14 +1,18 @@
 # -*- coding: utf-8 -*-
 
 """
-Functions to provide vtkCamera functionality.
+Functions to setup a VTK camera to match the OpenCV calibrated camera.
 """
 
 import vtk
 import numpy as np
 
+
 def create_vtk_matrix_4x4_from_numpy(array):
     """ Return a new vtkMatrix4x4 from a numpy array. """
+
+    if not isinstance(array, np.ndarray):
+        raise TypeError('Invalid array object passed')
 
     if array.shape != (4, 4):
         raise ValueError('Input array should be a 4x4 matrix')
@@ -17,6 +21,80 @@ def create_vtk_matrix_4x4_from_numpy(array):
     vtk_matrix.DeepCopy(array.ravel())
 
     return vtk_matrix
+
+
+def compute_projection_matrix_from_intrinsics(width,
+                                              height,
+                                              fx,
+                                              fy,
+                                              cx,
+                                              cy,
+                                              near,
+                                              far,
+                                              aspect_ratio):
+    """
+    Computes the OpenGL projection matrix.
+
+    Inspired by:
+    http://strawlab.org/2011/11/05/augmented-reality-with-OpenGL/
+
+    :param width: window width in pixels
+    :param height: window height in pixels
+    :param fx: focal length in x direction, (K_00)
+    :param fy: focal length in y direction, (K_11)
+    :param cx: principal point x coordinate, (K02)
+    :param cy: principal point y coordinate, (K12)
+    :param near: near clipping distance in world coordinate frame units (mm).
+    :param far:  far clipping distance in world coordinate frame units (mm).
+    :param aspect_ratio: relative physical size of pixels, as x/y.
+    :return: vtkMatrix4x4
+    """
+
+    matrix = vtk.vtkMatrix4x4()
+    matrix.Zero()
+    matrix.SetElement(0, 0, 2*fx/width)
+    matrix.SetElement(0, 1, -2*0/width)
+    matrix.SetElement(0, 2, (width - 2*cx)/width)
+    matrix.SetElement(1, 1, 2*(fy / aspect_ratio) / (height / aspect_ratio))
+    matrix.SetElement(1, 2, (-(height / aspect_ratio) + 2*(cy/aspect_ratio))/(height / aspect_ratio))
+    matrix.SetElement(2, 2, (-far-near)/(far-near))
+    matrix.SetElement(2, 3, -2*far*near/(far-near))
+    matrix.SetElement(3, 2, -1)
+    return matrix
+
+
+def set_camera_pose(vtk_camera, vtk_matrix):
+    """
+    Sets the camera position and orientation from a camera to world matrix.
+
+    :param vtk_camera: a vtkCamera
+    :param vtk_matrix: a vtkMatrix4x4 representing the camera to world.
+    :return:
+    """
+    if not isinstance(vtk_camera, vtk.vtkCamera):
+        raise TypeError('Invalid camera object passed')
+
+    if not isinstance(vtk_matrix, vtk.vtkMatrix4x4):
+        raise TypeError('Invalid matrix object passed')
+
+    # This implies a right handed coordinate system.
+    # By default, assume camera position is at origin,
+    # looking down the world +ve z-axis.
+    origin = [0, 0, 0, 1]
+    focal_point = [0, 0, 1000, 1]
+    view_up = [0, -1000, 0, 1]
+
+    vtk_matrix.MultiplyPoint(origin, origin)
+    vtk_matrix.MultiplyPoint(focal_point, focal_point)
+    vtk_matrix.MultiplyPoint(view_up, view_up)
+    view_up[0] = view_up[0] - origin[0]
+    view_up[1] = view_up[1] - origin[1]
+    view_up[2] = view_up[2] - origin[2]
+
+    # We then move the camera to that position.
+    vtk_camera.SetPosition(origin[0], origin[1], origin[2])
+    vtk_camera.SetFocalPoint(focal_point[0], focal_point[1], focal_point[2])
+    vtk_camera.SetViewUp(view_up[0], view_up[1], view_up[2])
 
 
 def set_projection_matrix(vtk_camera, vtk_matrix):
@@ -30,20 +108,3 @@ def set_projection_matrix(vtk_camera, vtk_matrix):
 
     vtk_camera.UseExplicitProjectionTransformMatrixOn()
     vtk_camera.SetExplicitProjectionTransformMatrix(vtk_matrix)
-
-
-def get_projection_matrix_from_intrinsics(vtk_camera, width, height, near, far):
-
-    aspect = width/height
-    vtk_matrix = vtk_camera.GetProjectionTransformMatrix(aspect, near, far)
-    print(vtk_matrix)
-    np_matrix = np.zeros((4,4))
-
-    for i in range(4):
-        for j in range(4):
-            np_matrix[i,j] = vtk_matrix.GetElement(i,j)
-
-    print(np_matrix)
-    return np_matrix
-
-
--- tests/camera/test_vtk_camera_model.py
+++ tests/camera/test_vtk_camera_model.py
@@ -1,13 +1,19 @@
 import pytest
 import vtk
-import six
 import numpy as np
 
 import sksurgeryvtk.camera.vtk_camera_model as cam
 
+
+def test_create_vtk_matrix_4x4_from_numpy_fail_on_invalid_type():
+
+    with pytest.raises(TypeError):
+        cam.create_vtk_matrix_4x4_from_numpy("hello")
+
+
 def test_create_vtk_matrix_4x4_from_numpy_fail_on_invalid_shape():
 
-    array = np .ones([2,3])
+    array = np .ones([2, 3])
 
     with pytest.raises(ValueError):
         cam.create_vtk_matrix_4x4_from_numpy(array)
@@ -15,48 +21,45 @@ def test_create_vtk_matrix_4x4_from_numpy_fail_on_invalid_shape():
 
 def test_create_vtk_matrix_4x4_from_numpy():
 
-    array = np.random.rand(4,4)
+    array = np.random.rand(4, 4)
 
     vtk_matrix = cam.create_vtk_matrix_4x4_from_numpy(array)
 
     for i in range(4):
         for j in range(4):
-            assert vtk_matrix.GetElement(i,j) == array[i,j]
+            assert vtk_matrix.GetElement(i, j) == array[i, j]
 
 
 def test_set_projection_matrix_fail_on_invalid_camera():
 
-    not_a_camera = np.ones([5,5])
+    not_a_camera = np.ones([5, 5])
     vtk_matrix = vtk.vtkMatrix4x4()
 
     with pytest.raises(TypeError):
         cam.set_projection_matrix(not_a_camera,  vtk_matrix)
 
 
-def test_set_projection_matrix_fail_on_invalid_camera():
+def test_set_projection_matrix_fail_on_invalid_matrix():
 
     camera = vtk.vtkCamera()
-    not_a_vtk_matrix = np.ones([5,5])
+    not_a_vtk_matrix = np.ones([5, 5])
 
     with pytest.raises(TypeError):
         cam.set_projection_matrix(camera,  not_a_vtk_matrix)
 
 
-def test_set_projection_matrix():
+def test_compute_projection_matrix_from_intrinsics():
 
+    matrix = cam.compute_projection_matrix_from_intrinsics(760, 570,   # w, y
+                                                           500, 400,   # fx, fy,
+                                                           395, 275,   # cx, cy,
+                                                           0.1, 1000,  # near, far
+                                                           1           # aspect ratio
+                                                           )
     camera = vtk.vtkCamera()
-    vtk_matrix = vtk.vtkMatrix4x4()
-    cam.set_projection_matrix(camera, vtk_matrix)
-    
-    assert camera.GetExplicitProjectionTransformMatrix() == vtk_matrix
 
+    cam.set_projection_matrix(camera, matrix)
 
-def test_get_projection_matrix_from_intrinsics():
-        camera = vtk.vtkCamera()
-        array = cam.get_projection_matrix_from_intrinsics(camera, 400, 400, 1, 0)
-
-        assert isinstance(array, np.ndarray)
-
-
-
+    output = camera.GetExplicitProjectionTransformMatrix()
+    assert matrix == output
 
