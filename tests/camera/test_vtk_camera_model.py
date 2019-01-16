7d52f4de860af5dff6a25a692518a661c774ddfa
Matt Clarkson <m.clarkson@ucl.ac.uk>
2019-01-16 17:14:24 +0000

    Issue #12: Add unit test data.

--- tests/camera/test_vtk_camera_model.py
+++ tests/camera/test_vtk_camera_model.py
@@ -50,11 +50,11 @@ def test_set_projection_matrix_fail_on_invalid_matrix():
 
 def test_compute_projection_matrix_from_intrinsics():
 
-    matrix = cam.compute_projection_matrix_from_intrinsics(760, 570,   # w, y
-                                                           500, 400,   # fx, fy,
-                                                           395, 275,   # cx, cy,
-                                                           0.1, 1000,  # near, far
-                                                           1           # aspect ratio
+    matrix = cam.compute_projection_matrix_from_intrinsics(1920, 1080,                # w, y
+                                                           2012.186314, 2017.966019,  # fx, fy,
+                                                           944.7173708, 617.1093984,  # cx, cy,
+                                                           0.1, 1000,                 # near, far
+                                                           1                          # aspect ratio
                                                            )
     camera = vtk.vtkCamera()
 
@@ -63,3 +63,11 @@ def test_compute_projection_matrix_from_intrinsics():
     output = camera.GetExplicitProjectionTransformMatrix()
     assert matrix == output
 
+
+def test_camera_projection():
+    # See data:
+    # chessboard_14_10_3.txt - 3D chessboard coordinates
+    # left-1095.png - image taken of chessboard
+    # left-1095.png.points.txt - detected 2D image points
+    # calib.intrinsic.txt - top 3x3 matrix are intrinsic parameters
+    # left-1095.extrinsic.txt - model to camera matrix, a.k.a camera extrinsics, a.k.a pose
