de9392e0b09c51d43259bb6627d1241feb6ce275
Matt Clarkson <m.clarkson@ucl.ac.uk>
2019-01-25 15:57:58 +0000

    Issue #33: Move sksurgeryrendermodelslikecamera to scikit-surgeryutils.

--- sksurgeryrendermodelslikecamera.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-#  -*- coding: utf-8 -*-
-import sys
-
-from sksurgeryvtk.ui.sksurgeryrendermodelslikecamera_command_line import main
-
-if __name__ == "__main__":
-    sys.exit(main(sys.argv[1:]))
