4d60ad2afa9ec93374ed4438fb175a87e67d90c7
t <tdowrick@gmail.com>
2019-02-05 15:42:26 +0000

    Issue #42: Fix test when no camera is available

--- tests/widgets/test_common_overlay_apps.py
+++ tests/widgets/test_common_overlay_apps.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import cv2
 import pytest
 import sksurgeryvtk.widgets.common_overlay_apps as coa
 
@@ -6,16 +7,21 @@ def test_OverlayOnVideoFeed(setup_qt):
     """
     Test will only run if there is a camera avilable.
     """
-    try:
-        overlay_app = coa.OverlayOnVideoFeed(0)
+
+    # Try to open a camera. If one isn't available, the rest of test
+    # will be skipped.
+    camera_source = 5
+    x = cv2.VideoCapture(camera_source)
+    if x.isOpened():
+        x.release()
+
+        overlay_app = coa.OverlayOnVideoFeed(camera_source)
         overlay_app.add_vtk_models_from_dir('tests/data/models/Liver')
         overlay_app.start()
         overlay_app.update()
         overlay_app.stop()
 
-    except IndexError:
-        # No cameras availble
-        return
+
 
 def test_OverlayBaseAppRaisesNotImplementedError(setup_qt):
     """
