06c0217d0d1587085f081c31ea40284fc646c061
Thomas Dowrick <t.dowrick@ucl.ac.uk>
2019-02-12 07:49:50 +0000

    Issue #48: Fix linting

--- sksurgeryvtk/widgets/common_overlay_apps.py
+++ sksurgeryvtk/widgets/common_overlay_apps.py
@@ -1,10 +1,14 @@
 """Common use cases for vtk_overlay_window"""
 
-#pylint: disable=no-member, no-name-in-module, protected-access
+#pylint: disable=unused-import
 # coding=utf-8
-
-print("WARNING: common_overlay_apps has benn moved to scikit-surgeryutils. Please update your imports")
-print("WARNING: common_overlay_apps has benn moved to scikit-surgeryutils. Please update your imports")
-print("WARNING: common_overlay_apps has benn moved to scikit-surgeryutils. Please update your imports")
-
 from sksurgeryutils import common_overlay_apps
+
+print("WARNING: common_overlay_apps/OverlayBaseApp \
+    has benn moved to scikit-surgeryutils. Please update your imports")
+
+print("WARNING: common_overlay_apps/OverlayBaseApp \
+    has benn moved to scikit-surgeryutils. Please update your imports")
+
+print("WARNING: common_overlay_apps/OverlayBaseApp \
+    has benn moved to scikit-surgeryutils. Please update your imports")
