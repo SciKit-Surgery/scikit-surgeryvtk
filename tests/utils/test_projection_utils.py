8eee7f8d8498708d3dc6e9a63d2d30526fb8cf4d
Thomas Dowrick <tdowrick@gmail.com>
2019-03-20 14:19:31 +0000

    Issue #55: Use identity matrix as world_to_camera rather than np.zeros() so that matricies are rigid

--- tests/utils/test_projection_utils.py
+++ tests/utils/test_projection_utils.py
@@ -5,7 +5,6 @@ import numpy as np
 
 import sksurgeryvtk.utils.projection_utils as pu
 
-
 def test_project_points_invalid_as_input_null():
 
     with pytest.raises(ValueError):
@@ -21,73 +20,73 @@ def test_project_points_invalid_as_input_null():
 def test_project_points_invalid_as_points_not_numpy_array():
 
     with pytest.raises(TypeError):
-        pu.project_points("invalid", np.zeros([4, 4]), np.zeros([3, 3]), None)
+        pu.project_points("invalid", np.zeros([4, 4]), np.eye(3), None)
 
 
 def test_project_points_invalid_as_points_not_2d_matrix():
 
     with pytest.raises(ValueError):
-        pu.project_points(np.zeros([4, 3, 4]), np.zeros([4, 4]), np.zeros([3, 3]), None)
+        pu.project_points(np.zeros([4, 3, 4]), np.zeros([4, 4]), np.eye(3), None)
 
 
 def test_project_points_invalid_as_points_not_3_columns():
 
     with pytest.raises(ValueError):
-        pu.project_points(np.zeros([4, 4]), np.zeros([4, 4]), np.zeros([3, 3]), None)
+        pu.project_points(np.zeros([4, 4]), np.zeros([4, 4]), np.eye(3), None)
 
 
 def test_project_points_invalid_as_extrinsics_not_numpy_array():
 
     with pytest.raises(TypeError):
-        pu.project_points(np.zeros([4, 3]), "invalid", np.zeros([3, 3]), None)
+        pu.project_points(np.zeros([4, 3]), "invalid", np.eye(3), None)
 
 
 def test_project_points_invalid_as_extrinsics_not_2d():
 
     with pytest.raises(ValueError):
-        pu.project_points(np.zeros([4, 3]), np.zeros([4, 4, 4]), np.zeros([3, 3]), None)
+        pu.project_points(np.zeros([4, 3]), np.zeros([4, 4, 4]), np.eye(3), None)
 
 
 def test_project_points_invalid_as_extrinsics_wrong_number_rows():
 
     with pytest.raises(ValueError):
-        pu.project_points(np.zeros([4, 3]), np.zeros([5, 4]), np.zeros([3, 3]), None)
+        pu.project_points(np.zeros([4, 3]), np.zeros([5, 4]), np.eye(3), None)
 
 
 def test_project_points_invalid_as_extrinsics_wrong_number_columns():
 
     with pytest.raises(ValueError):
-        pu.project_points(np.zeros([4, 3]), np.zeros([4, 5]), np.zeros([3, 3]), None)
+        pu.project_points(np.zeros([4, 3]), np.zeros([4, 5]), np.eye(3), None)
 
 
 def test_project_points_invalid_as_intrinsics_wrong_size():
 
     with pytest.raises(ValueError):
-        pu.project_points(np.zeros([4, 3]), np.zeros([4, 4]), np.zeros([2, 2]), np.zeros([1, 4]))
+        pu.project_points(np.zeros([4, 3]), np.zeros([4, 4]), np.eye(2), np.zeros([1, 4]))
 
 
 def test_project_points_invalid_as_distortion_wrong_number_rows():
 
     with pytest.raises(ValueError):
-        pu.project_points(np.zeros([4, 3]), np.zeros([4, 4]), np.zeros([3, 3]), np.zeros([2, 4]))
+        pu.project_points(np.zeros([4, 3]), np.eye(4), np.eye(3), np.zeros([2, 4]))
 
 
 def test_project_facing_points_invalid_as_normals_null():
 
     with pytest.raises(ValueError):
-        pu.project_facing_points(np.zeros([4, 3]), None, np.zeros([4, 4]), np.zeros([3, 3]))
+        pu.project_facing_points(np.zeros([4, 3]), None, np.eye(4), np.zeros([3, 3]))
 
 
 def test_project_facing_points_invalid_as_normals_not_numpy_array():
 
     with pytest.raises(TypeError):
-        pu.project_facing_points(np.zeros([4, 3]), "hello", np.zeros([4, 4]), np.zeros([3, 3]))
+        pu.project_facing_points(np.zeros([4, 3]), "hello", np.eye(4), np.zeros([3, 3]))
 
 
 def test_project_facing_points_invalid_as_normals_shape_doesnt_match_points():
 
     with pytest.raises(ValueError):
-        pu.project_facing_points(np.zeros([4, 3]), np.zeros([4, 4]), np.zeros([4, 4]), np.zeros([3, 3]))
+        pu.project_facing_points(np.zeros([4, 3]), np.zeros([4, 4]), np.eye(4), np.zeros([3, 3]))
 
 
 def test_project_facing_points_valid_example_synthetic_data():
