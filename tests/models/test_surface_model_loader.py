4fcf190934ff86386410b8621e3bb5b86cd9079e
Matt Clarkson <m.clarkson@ucl.ac.uk>
2019-03-28 06:50:48 +0000

    Issue #59: Added stubs for surface_model_loader.py

---
+++ tests/models/test_surface_model_loader.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+import pytest
+from sksurgeryvtk.models.vtk_surface_model import VTKSurfaceModel
+from sksurgerycore.configuration.configuration_manager import ConfigurationManager
+
+
+def test_empty_surface_config():
+    pass
+
+def test_two_surfaces_no_assembly():
+    pass
+
+
+def test_two_surface_in_assembly():
+    pass
+
+
+def test_one_surface_one_assembly_with_two_surfaces():
+    pass
