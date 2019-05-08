b0661ec1496d239f7311ec13a7863e1789054581
BongjinKoo <b.koo@ucl.ac.uk>
2019-05-08 16:07:33 +0100

    Issue #64: Implement voxelisation of a 3D model (currently only from an STL file).

---
+++ tests/utils/test_voxelisation_utils.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+import pytest
+import vtk
+import numpy as np
+from vtk.util import colors
+from sksurgeryvtk.models.vtk_surface_model import VTKSurfaceModel
+import sys
+import os
+
+import sksurgeryvtk.utils.voxelisation_utils as vu
+
+
+def test_voxelisation_of_3d_mesh(vtk_overlay_with_gradient_image):
+    input_file = 'tests/data/models/spacefighter.stl'
+    glyph_3d_mapper = vu.voxelise_3d_mesh(input_file, 200, 200, 200)
+    actor = vtk.vtkActor()
+    actor.SetMapper(glyph_3d_mapper)
+    actor.GetProperty().SetColor(0.0, 1.0, 0.0)
+    image, widget, _, _, app = vtk_overlay_with_gradient_image
+    widget.add_vtk_actor(actor)
+    widget.show()
+
+    # Uncomment if you want to check the voxelised model.
+    # app.exec_()
+
