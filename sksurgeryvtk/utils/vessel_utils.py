bbf6c4e738b1c1a2dc6c7a838132a516504579aa
BongjinKoo <b.koo@ucl.ac.uk>
2019-05-30 15:54:17 +0100

    Issue #66: Implement loading/displaying vessel centrelines and branches from a file: using Accurate Fast Matching, in place of VMTK.

--- sksurgeryvtk/utils/vessel_utils.py
+++ sksurgeryvtk/utils/vessel_utils.py
@@ -7,6 +7,7 @@ which currently are the centrelines computed by a VMTK script
 """
 import os
 import vtk
+import random
 import numpy as np
 import sksurgerycore.utilities.validate_file as f
 
@@ -15,10 +16,12 @@ import sksurgerycore.utilities.validate_file as f
 
 def load_vessel_centrelines(file_name):
     """
-    Loads vessel centrelines from a file generated from VMTK
-    (vmtkcenterlines) and converts them into vtkPolyData.
+    Loads vessel centrelines from a file generated using
+    'Accurate Fast Matching (https://uk.mathworks.com/matlabcentral/fileexchange/24531-accurate-fast-marching)'
+    and converts them into vtkPolyData.
 
-    :param file_name: A path to the vessel centrelines file
+    :param file_name: A path to the vessel centrelines file (.txt),
+                      which contains vessel centrelines and branch infomation.
 
     :return: poly_data: vtkPolyData containing the vessel centrelines
              poly_data_mapper: vtkPolyDataMapper for the vessel centrelines
@@ -26,73 +29,209 @@ def load_vessel_centrelines(file_name):
 
     f.validate_is_file(file_name)
 
-    positions = []
+    poly_data = vtk.vtkPolyData()
+    points = vtk.vtkPoints()
+    line_indices = vtk.vtkCellArray()
+    colours = vtk.vtkUnsignedCharArray()
+    colours.SetNumberOfComponents(3)
+    colours.SetName('colours')
+    point_index = 0
 
-    with open(file_name, 'r') as read_file:
+    with open(file_name, 'r') as file:
         # Check if the file is empty.
         if os.path.getsize(file_name) == 0:
             raise ValueError('The file is empty.')
 
-        line_index = 1
+        # Branches.
+        print(file.readline().strip())
 
-        for line in read_file:
-            # Each line in the vessel centrelines file contains:
-            # X Y Z
-            # MaximumInscribedSphereRadius
-            # EdgeArray0
-            # EdgeArray1
-            # EdgePCoordArray.
-            # Here we only use the 3D position of a point in the line, i.e.,
-            # X, Y, Z.
+        # Number of branches.
+        print(file.readline().strip())
 
-            # First line is a comment.
-            if line_index == 1:
-                line_index += 1
-                continue
+        number_of_branches = int(file.readline().strip())
+        print(number_of_branches)
 
-            # Each token is separated by a space.
-            x, y, z, _, _, _, _ = line.split(' ')
-            positions.append([x, y, z])
+        for i in range(number_of_branches):
+            # Branch ID.
+            print(file.readline().strip())
 
-            line_index += 1
+            # Number of points.
+            print(file.readline().strip())
+            number_of_points = int(file.readline().strip())
+            print(number_of_points)
 
-        # Convert the list of positions into a numpy array.
-        positions = np.array(positions, dtype=float)
+            # Points.
+            print(file.readline().strip())
 
-    poly_data = vtk.vtkPolyData()
-    points = vtk.vtkPoints()
-    points.SetData(vtk.util.numpy_support.numpy_to_vtk(positions))
+            # Create a line for each branch.
+            line_indices.InsertNextCell(number_of_points)
+
+            for j in range(number_of_points):
+                x, y, z = file.readline().strip().split(',')
+                points.InsertNextPoint([float(x), float(y), float(z)])
+                line_indices.InsertCellPoint(point_index + j)
+
+            point_index += number_of_points
+
+            # Set a random colour for each branch
+            r = random.randint(1, 256)
+            g = random.randint(1, 256)
+            b = random.randint(1, 256)
+            colours.InsertNextTuple3(r, g, b)
+
+
     poly_data.SetPoints(points)
+    poly_data.SetLines(line_indices)
+    poly_data.GetCellData().SetScalars(colours)
 
-    centre_of_mass_filter = vtk.vtkCenterOfMass()
-    centre_of_mass_filter.SetInputData(poly_data)
-    centre_of_mass_filter.SetUseScalarsAsWeights(False)
-    centre_of_mass_filter.Update()
-    centre = centre_of_mass_filter.GetCenter()
-    centre = np.array(centre)
 
-    new_points = vtk.vtkPoints()
-    number_of_points = poly_data.GetNumberOfPoints()
+            # # Each token is separated by a space.
+            # x, y, z, _, _, _, _ = line.split(' ')
+            # positions.append([x, y, z])
 
-    # Current scale factor is empirically set.
-    # The liver model is not scaled.
-    scale = 1
 
-    for i in range(number_of_points):
-        point = poly_data.GetPoint(i)
-        # point = point - centre
-        new_points.InsertNextPoint([point[0] * scale,
-                                    point[1] * scale,
-                                    point[2] * scale])
 
-    poly_data.SetPoints(new_points)
+    # points = vtk.vtkPoints()
+    # points.SetData(vtk.util.numpy_support.numpy_to_vtk(positions))
+    # poly_data.SetPoints(points)
+    #
+    # centre_of_mass_filter = vtk.vtkCenterOfMass()
+    # centre_of_mass_filter.SetInputData(poly_data)
+    # centre_of_mass_filter.SetUseScalarsAsWeights(False)
+    # centre_of_mass_filter.Update()
+    # centre = centre_of_mass_filter.GetCenter()
+    # centre = np.array(centre)
+    #
+    # new_points = vtk.vtkPoints()
+    # number_of_points = poly_data.GetNumberOfPoints()
+    #
+    # # Current scale factor is empirically set.
+    # # The liver model is not scaled.
+    # scale = 1
+    #
+    # for i in range(number_of_points):
+    #     point = poly_data.GetPoint(i)
+    #     # point = point - centre
+    #     new_points.InsertNextPoint([point[0] * scale,
+    #                                 point[1] * scale,
+    #                                 point[2] * scale])
+    #
+    # poly_data.SetPoints(new_points)
+    #
+    # lines = vtk.vtkCellArray()
+    # lines.InsertNextCell(number_of_points)
+    # for i in range(number_of_points):
+    #     lines.InsertCellPoint(i)
+    #
+    # poly_data.SetLines(lines)
 
-    lines = vtk.vtkCellArray()
-    lines.InsertNextCell(number_of_points)
-    for i in range(number_of_points):
-        lines.InsertCellPoint(i)
 
-    poly_data.SetLines(lines)
+
+
+
+    # poly_data_reader = vtk.vtkXMLPolyDataReader()
+    # poly_data_reader.SetFileName(file_name)
+    # poly_data_reader.Update()
+    # poly_data = poly_data_reader.GetOutput()
+    # poly_data.BuildLinks()
+    #
+    # number_of_points = poly_data.GetNumberOfPoints()
+    # if number_of_points == 0:
+    #     raise ValueError('There are no points.')
+    #
+    # points = poly_data.GetPoints()
+    # if points is None:
+    #     raise ValueError('points is None.')
+    #
+    # number_of_cells = poly_data.GetNumberOfCells()
+    # print('number of cells', number_of_cells)
+    #
+    # array = poly_data.GetCellData().GetAbstractArray('GroupIds')
+    #
+    # # for i in range(number_of_cells):
+    # #     cell = poly_data.GetCell(i)
+    #
+    # colours = vtk.vtkUnsignedCharArray()
+    # colours.SetNumberOfComponents(3)
+    # colours.SetName('Colours')
+    #
+    # group_ids = []
+    # group_colour_array = np.zeros([array.GetSize(), 3])
+    # colour_array = np.zeros([number_of_cells, 3])
+    #
+    # for i in range(number_of_cells):
+    #     group_id = array.GetValue(i)
+    #
+    #     # Add the id only if it has not been added to the list.
+    #     if group_id in group_ids:
+    #         # index = group_ids.index(group_id)
+    #         colour_array[i, :] = group_colour_array[group_id, :]
+    #         continue
+    #
+    #     r = random.randint(1, 256)
+    #     g = random.randint(1, 256)
+    #     b = random.randint(1, 256)
+    #     group_colour_array[group_id, :] = [r, g, b]
+    #     colour_array[i, :] = [r, g, b]
+    #
+    #     group_ids.append(group_id)
+    #
+    # for i in range(number_of_cells):
+    #     colours.InsertNextTuple3(colour_array[i, 0], colour_array[i, 1],
+    #                              colour_array[i, 2])
+    #
+    #
+    #     # points_in_a_cell = vtk.vtkIdList()
+    #     # poly_data.GetCellPoints(i, points_in_a_cell)
+    #     # number_of_points_in_a_cell = points_in_a_cell.GetNumberOfIds()
+    #
+    # poly_data.GetCellData().SetScalars(colours)
+
+        # point = points.GetPoint(i)
+
+        # cell_ids = vtk.vtkIdList()
+        # poly_data.GetPointCells(i, cell_ids)
+        # number_of_cell_ids = cell_ids.GetNumberOfIds()
+        # print('number of cell ids', number_of_cell_ids)
+    #
+    #     for j in range(number_of_cell_ids):
+    #         points_in_a_cell = vtk.vtkIdList()
+    #         poly_data.GetCellPoints(cell_ids.GetId(j), points_in_a_cell)
+    #         number_of_points_in_a_cell = points_in_a_cell.GetNumberOfIds()
+    #         # print('num of points in a cell', number_of_points_in_a_cell)
+    #         # print('id', cell_ids.GetId(j))
+    #
+    #         print(poly_data.GetCell(cell_ids.GetId(j)))
+
+        # print(array.GetTuple1(i))
+
+    #     print('Group ID', array.GetValue(i))
+
+    #     cell_ids = vtk.vtkIdList()
+    #     poly_data.GetPointCells(i, cell_ids)
+    #     number_of_cell_ids = cell_ids.GetNumberOfIds()
+    #
+    #     after = []
+    #
+    #     for j in range(number_of_cell_ids):
+    #         points_in_a_cell = vtk.vtkIdList()
+    #         poly_data.GetCellPoints(cell_ids.GetId(j), points_in_a_cell)
+    #
+    #         number_of_points_in_a_cell = points_in_a_cell.GetNumberOfIds()
+    #
+    #         for k in range(number_of_points_in_a_cell):
+    #             if points_in_a_cell.GetId(k) == i and \
+    #                     k != (number_of_points_in_a_cell - 1):
+    #                 after.append(points_in_a_cell.GetId(k + 1))
+    #
+    #     if len(after) > 1:
+    #         for id in after:
+    #             point = points.GetPoint(id)
+    #             print('added point id', id)
+
+
+
+
 
     poly_data_mapper = vtk.vtkPolyDataMapper()
     poly_data_mapper.SetInputData(poly_data)
--- tests/utils/test_vessel_utils.py
+++ tests/utils/test_vessel_utils.py
@@ -13,7 +13,7 @@ import sksurgeryvtk.utils.vessel_utils as vu
 
 def test_load_vessel_centrelines_from_file_valid(
         vtk_overlay_with_gradient_image):
-    input_file = 'tests/data/vessel_centrelines/vessel_centrelines.dat'
+    input_file = 'tests/data/vessel_centrelines/vessel_tree_info.txt'
     poly_data, poly_data_mapper = vu.load_vessel_centrelines(input_file)
 
     actor = vtk.vtkActor()
@@ -27,7 +27,7 @@ def test_load_vessel_centrelines_from_file_valid(
     # Uncomment if you want to check the voxelised model.
     # app.exec_()
 
-def test_load_vessel_centrelines_from_file_invalid_as_empty():
-    input_file = 'tests/data/vessel_centrelines/vessel_centrelines_empty.dat'
-    with pytest.raises(ValueError):
-        vu.load_vessel_centrelines(input_file)
+# def test_load_vessel_centrelines_from_file_invalid_as_empty():
+#     input_file = 'tests/data/vessel_centrelines/vessel_centrelines_empty.dat'
+#     with pytest.raises(ValueError):
+#         vu.load_vessel_centrelines(input_file)
