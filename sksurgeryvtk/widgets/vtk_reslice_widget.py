f7400c13668fcfc30eb845f901c8b90ffe09ed18
Thomas Dowrick <tdowrick@gmail.com>
2020-01-09 15:32:36 +0000

    WIP: Add 3D view of all slices in 4th widget

--- sksurgeryvtk/widgets/vtk_reslice_widget.py
+++ sksurgeryvtk/widgets/vtk_reslice_widget.py
@@ -11,7 +11,7 @@ from sksurgeryvtk.widgets.QVTKRenderWindowInteractor import QVTKRenderWindowInte
 import cv2
 import cv2.aruco as aruco
 
-class aruco_tracker:
+class ArucoTracker:
 
     def __init__(self, image_source):
         #the aruco tag dictionary to use. DICT_4X4_50 will work with the tag in
@@ -59,7 +59,8 @@ class aruco_tracker:
 
 
 class VTKSliceViewer(QtWidgets.QWidget):
-    """ Othrogonal slice viewer showing Axial/Sagittal/Coronal views """
+    """ Othrogonal slice viewer showing Axial/Sagittal/Coronal views
+    :param dicom_dir: path to folder containig dicom data """
 
     def __init__(self, dicom_dir): 
 
@@ -72,50 +73,141 @@ class VTKSliceViewer(QtWidgets.QWidget):
         self.reader.Update()
 
         self.frame = QtWidgets.QFrame()
-        self.axial = VTKResliceWidget(self.reader, 'axial', self.frame)
-        self.sagittal = VTKResliceWidget(self.reader, 'sagittal', self.frame)
-        self.coronal = VTKResliceWidget(self.reader, 'coronal', self.frame)
+        self.x = SimpleVTKResliceWidget(self.reader, 'x', self.frame)
+        self.y = SimpleVTKResliceWidget(self.reader, 'y', self.frame)
+        self.z = SimpleVTKResliceWidget(self.reader, 'z', self.frame)
 
-        self.layout.addWidget(self.axial, 0, 0)
-        self.layout.addWidget(self.sagittal, 0, 1)
-        self.layout.addWidget(self.coronal, 1, 0)
-
-        self.fourth_panel = QtWidgets.QLabel("Placeholder")
-        self.layout.addWidget(self.fourth_panel, 1, 1)
+        self.layout.addWidget(self.x, 0, 0)
+        self.layout.addWidget(self.y, 0, 1)
+        self.layout.addWidget(self.z, 1, 0)
 
         self.setLayout(self.layout)
-        self.axial.GetRenderWindow().Render()
-        self.sagittal.GetRenderWindow().Render()
-        self.coronal.GetRenderWindow().Render()
+        self.x.GetRenderWindow().Render()
+        self.y.GetRenderWindow().Render()
+        self.z.GetRenderWindow().Render()
 
+        self.fourth_panel = QVTKRenderWindowInteractor(self.frame)
+       
+        self.renderer = vtk.vtkRenderer()
+        self.renderer.SetBackground(.1, .2, .1)
+        self.fourth_panel.GetRenderWindow().AddRenderer(self.renderer)
 
-    def update_slice_positions(self, x, y, z):
+        for v in [self.x, self.y, self.z]:
+            self.renderer.AddActor(v.actor)
+
+        self.layout.addWidget(self.fourth_panel, 1, 1)
+        self.fourth_panel.GetRenderWindow().Render()
+
+    def update_slice_positions(self, x_pos, y_pos, z_pos):
         """ Set the slice positions for each view.
         :param x: slice 1 position
         :param y: slice 2 position
         :param z: slice 3 position
         """
-        self.axial.set_slice_position(z)
-        self.sagittal.set_slice_position(x)
-        self.coronal.set_slice_position(y)
+        self.z.set_slice_position(z_pos)
+        self.x.set_slice_position(x_pos)
+        self.y.set_slice_position(y_pos)
 
+
+        self.fourth_panel.GetRenderWindow().Render()
 
 
     def reset_slice_positions(self):
         """ Set slcie positions to some default values. """
         self.update_slice_positions(0, 0, 0)
 
+class SimpleVTKResliceWidget(QVTKRenderWindowInteractor):
+    
+    def __init__(self, reader, axis, parent):
+
+        if axis not in ['x', 'y', 'z']:
+            raise TypeError('Argument should be axial/sagittal/coronal')
+
+        super().__init__(parent)
+        self.axis = axis
+
+         # Calculate the center of the volume
+        (self.xMin, self.xMax, self.yMin, self.yMax, self.zMin, self.zMax) = \
+             reader.GetExecutive().GetWholeExtent(reader.GetOutputInformation(0))
+
+        (self.xSpacing, self.ySpacing, self.zSpacing) = reader.GetOutput().GetSpacing()
+        # (self.x0, self.y0, self.z0) = reader.GetOutput().GetOrigin()
+
+        # self.center = [self.x0 + self.xSpacing * 0.5 * (self.xMin + self.xMax),
+        #         self.y0 + self.ySpacing * 0.5 * (self.yMin + self.yMax),
+        #         self.z0 + self.zSpacing * 0.5 * (self.zMin + self.zMax)]
+        print(f'x: {self.xMin} {self.xMax} {self.zSpacing}\n \
+         y:{self.yMin} {self.yMax} {self.ySpacing}\n \
+         z: {self.zMin} {self.zMax} {self.zSpacing}')
+        self.lut = vtk.vtkLookupTable()
+        self.lut.SetTableRange(-1000,1000)
+        self.lut.SetHueRange(0,0)
+        self.lut.SetSaturationRange(0,0)
+        self.lut.SetValueRange(0,1)
+        self.lut.Build()
+
+        self.colours = vtk.vtkImageMapToColors()
+        self.colours.SetInputConnection(reader.GetOutputPort())
+        self.colours.SetLookupTable(self.lut)
+        self.colours.Update()
+
+        self.actor = vtk.vtkImageActor()
+        self.actor.GetMapper().SetInputConnection(self.colours.GetOutputPort())
+
+        self.text_actor = vtk.vtkTextActor()
+        self.text_actor.SetInput(self.axis)
+
+        self.renderer = vtk.vtkRenderer()
+        self.renderer.AddActor(self.actor)
+        self.renderer.AddActor(self.text_actor)
+
+        self.set_slice_position(0)
+        self.renderer.ResetCamera(self.actor.GetBounds())
+        if axis == 'y':
+            self.renderer.GetActiveCamera().SetViewUp(0.0, 0.0, 1.0)
+        self.GetRenderWindow().AddRenderer(self.renderer)
+
+    def set_slice_position(self, position):
+        """ Set the slice position in the volume """
+
+        position = int(position)
+
+        if self.axis == 'x':
+            position = np.clip(position, self.xMin, self.xMax)
+            self.actor.SetDisplayExtent(position, position, self.yMin, self.yMax, self.zMin, self.zMax)
+
+        if self.axis == 'y':
+            position = np.clip(position, self.yMin, self.yMax)
+            self.actor.SetDisplayExtent(self.xMin, self.xMax, position, position, self.zMin, self.zMax)
+
+        if self.axis == 'z':
+            position = position // 10
+            position = np.clip(position, self.zMin, self.zMax)
+            self.actor.SetDisplayExtent(self.xMin, self.xMax, self.yMin, self.yMax, position, position)
+       
+        # print(self.axis)
+        # print(cam.GetViewUp())
+        # print(cam.GetFocalPoint())
+        # print(cam.GetPosition())
+
+        #self.renderer.ResetCamera(self.actor.GetBounds())
+        cam = self.renderer.GetActiveCamera()
+
+
+        self.GetRenderWindow().Render()
+
+
 class VTKResliceWidget(QVTKRenderWindowInteractor):
     """" Single slice view
     :param reader: vtk reader object containing data to be displayed
     :param plane_matrix: matrix which sets the slice orientation
+    :param parent: parent widget used in init method of QVTKRenderWindowInteractor
     """
 
-    def __init__(self, reader, plane, parent):
+    def __init__(self, reader, axis, parent):
         super().__init__(parent)
-        self.frame = QtWidgets.QFrame()
 
-        self.plane = plane
+        self.axis = axis
         # Calculate the center of the volume
         (self.xMin, self.xMax, self.yMin, self.yMax, self.zMin, self.zMax) = \
              reader.GetExecutive().GetWholeExtent(reader.GetOutputInformation(0))
@@ -127,39 +219,39 @@ class VTKResliceWidget(QVTKRenderWindowInteractor):
                 self.y0 + ySpacing * 0.5 * (self.yMin + self.yMax),
                 self.z0 + zSpacing * 0.5 * (self.zMin + self.zMax)]
 
-        # Matrices for axial, coronal, sagittal view orientations
-        axial = vtk.vtkMatrix4x4()
-        axial.DeepCopy((1, 0, 0, center[0],
+        # Matrices for x, y and z view orientations
+        y = vtk.vtkMatrix4x4()
+        y.DeepCopy((1, 0, 0, center[0],
                         0, 1, 0, center[1],
                         0, 0, 1, center[2],
                         0, 0, 0, 1))
 
-        coronal = vtk.vtkMatrix4x4()
-        coronal.DeepCopy((1, 0, 0, center[0],
+        x = vtk.vtkMatrix4x4()
+        x.DeepCopy((1, 0, 0, center[0],
                         0, 0, 1, center[1],
                         0,-1, 0, center[2],
                         0, 0, 0, 1))
 
-        sagittal = vtk.vtkMatrix4x4()
-        sagittal.DeepCopy((0, 0,-1, center[0],
+        z = vtk.vtkMatrix4x4()
+        z.DeepCopy((0, 0,-1, center[0],
                         1, 0, 0, center[1],
                         0,-1, 0, center[2],
                         0, 0, 0, 1))
 
-        if plane == 'axial':
-            plane_matrix = axial
+        if plane == 'y':
+            plane_matrix = y
             self.lower, self.upper = self.yMin, self.yMax
             self.spacing = ySpacing
             self.origin = self.y0
         
-        elif plane == 'sagittal':
-            plane_matrix = sagittal
+        elif plane == 'z':
+            plane_matrix = z
             self.lower, self.upper = self.zMin, self.zMax
             self.spacing = zSpacing
             self.origin = self.z0
 
-        elif plane == 'coronal':
-            plane_matrix = coronal
+        elif plane == 'x':
+            plane_matrix = x
             self.lower, self.upper = self.xMin, self.xMax
             self.spacing = xSpacing
             self.origin = self.x0
@@ -210,18 +302,20 @@ class VTKResliceWidget(QVTKRenderWindowInteractor):
         self.reslice.Update()
         matrix = self.reslice.GetResliceAxes()
 
-        if self.plane == 'sagittal':
-            matrix.SetElement(0, 3, position) #sagittal
-        if self.plane == 'coronal':
-            matrix.SetElement(1, 3, position) #coronal
-        if self.plane == 'axial':
-            matrix.SetElement(2, 3, position) #axial
+        if self.axis == 'z':
+            matrix.SetElement(0, 3, position)
+        if self.axis == 'x':
+            matrix.SetElement(1, 3, position)
+        if self.axis == 'y':
+            matrix.SetElement(2, 3, position)
         self.GetRenderWindow().Render()
 
 
 class TrackedSliceViewer(VTKSliceViewer):
     """ Orthogonal slice viewer combined with tracker to
-    control slice position. """
+    control slice position.
+    :param dicom_dir: Path to folder containing dicom data.
+    :param tracker: Tracker object used to control slice positions """
     def __init__(self, dicom_dir, tracker):
 
         super().__init__(dicom_dir)
@@ -252,8 +346,9 @@ class TrackedSliceViewer(VTKSliceViewer):
 
 qApp = QtWidgets.QApplication([])
 
-tracker = aruco_tracker(0)
-slice_viewer = TrackedSliceViewer('tests/data/dicom/LegoPhantom', tracker)
+tracker = ArucoTracker(0)
+dicom_path = 'tests/data/dicom/LegoPhantom'
+slice_viewer = TrackedSliceViewer(dicom_path, tracker)
 slice_viewer.start()
 
 qApp.exec_()
@@ -266,4 +361,4 @@ qApp.exec_()
 #     percentY = 100.0 * mouseY/screenY
 
 # position = self.lower + self.spacing * (self.upper - self.lower) * percent / 100
-# print(f'{self.plane} {self.upper} Position: {position}')
+# print(f'{self.axis} {self.upper} Position: {position}')
