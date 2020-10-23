f69d3df3fe2fcbd29a3f128e86cdf2b22300f9f2
Matt Clarkson <m.clarkson@ucl.ac.uk>
2020-10-23 15:59:35 +0100

    Issue #158: Formatting.

--- tests/text/test_corner_annotaiton.py
+++ tests/text/test_corner_annotaiton.py
@@ -9,20 +9,24 @@ def corner_annotaiton():
     corner_annotaiton = VTKCornerAnnotation()
     return corner_annotaiton
 
+
 def test_non_list_input_raises_error(corner_annotaiton):
     invalid_input = 1
     with pytest.raises(TypeError):
         corner_annotaiton.set_text(invalid_input)
+
 
 def test_invalid_size_input_raises_error(corner_annotaiton):
     invalid_input = ["1", "2", "3"]
     with pytest.raises(ValueError):
         corner_annotaiton.set_text(invalid_input)
 
+
 def test_non_string_in_list_raises_error(corner_annotaiton):
     invalid_input = ["1", "2", 3, "4"]
     with pytest.raises(ValueError):
         corner_annotaiton.set_text(invalid_input)
+
 
 def test_valid_input_raises_no_error(corner_annotaiton):
 
