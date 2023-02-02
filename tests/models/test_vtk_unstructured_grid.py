af991a9d7e36cfd439f5bb97076f0e7913aece19
Miguel Xochicale <m.xochicale@ucl.ac.uk>
2023-02-02 14:23:28 +0000

    :heavy_check_mark: local tests/models/test_vtk_unstructured_grid.py (#203 #187)

--- tests/models/test_vtk_unstructured_grid.py
+++ tests/models/test_vtk_unstructured_grid.py
@@ -1,21 +1,19 @@
+# -*- coding: utf-8 -*-
+
 import sksurgeryvtk.models.vtk_grid_model as GM
-import vtk
-import numpy as np
+
 
 def test_load_from_vtu():
-
     model = "tests/data/models/unstructured_grid.vtu"
     neonate_model = GM.VTKUnstructuredGridModel(model)
 
     array_min, array_max = neonate_model.get_cell_array_bounds()
     neonate_model.threshold_between(array_min, array_max)
 
-def test_load_from_vtk():
 
+def test_load_from_vtk():
     model = "tests/data/models/unstructured_grid.vtk"
     neonate_model = GM.VTKUnstructuredGridModel(model)
 
     array_min, array_max = neonate_model.get_cell_array_bounds()
     neonate_model.threshold_between(array_min, array_max)
-
-
