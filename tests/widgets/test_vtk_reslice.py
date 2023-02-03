e4aee992d93e89deab68b956b4200d322c0204af
Miguel Xochicale <m.xochicale@ucl.ac.uk>
2023-02-03 17:37:55 +0000

    :x: adds local tests/widgets/test_vtk_reslice.py (#203, #187)

--- tests/widgets/test_vtk_reslice.py
+++ tests/widgets/test_vtk_reslice.py
@@ -1,23 +1,21 @@
-import pytest
+# -*- coding: utf-8 -*-
+
 from sksurgeryvtk.widgets import vtk_reslice_widget
 
 
 def test_slice_viewer(qtbot):
-
     dicom_path = 'tests/data/dicom/LegoPhantom_10slices'
     reslice = vtk_reslice_widget.VTKSliceViewer(dicom_path)
 
     qtbot.addWidget(reslice)
 
-    reslice.update_slice_positions_pixels(1,1,1)
+    reslice.update_slice_positions_pixels(1, 1, 1)
 
 
 def test_mouse_scroll_slice_viewer(qtbot):
-
     dicom_path = 'tests/data/dicom/LegoPhantom_10slices'
     reslice = vtk_reslice_widget.MouseWheelSliceViewer(dicom_path)
 
     qtbot.addWidget(reslice)
 
-    reslice.update_slice_positions_pixels(1,1,1)
-
+    reslice.update_slice_positions_pixels(1, 1, 1)
