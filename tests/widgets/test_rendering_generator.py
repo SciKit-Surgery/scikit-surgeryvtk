f59e96f19c9b7146bf6e15f263710968f5957c25
Miguel Xochicale <m.xochicale@ucl.ac.uk>
2023-02-03 17:19:13 +0000

    commented test_basic_rendering_generator > tests/widgets/test_rendering_generator.py (#203, #187)

--- tests/widgets/test_rendering_generator.py
+++ tests/widgets/test_rendering_generator.py
@@ -8,75 +8,75 @@ import cv2
 import sksurgeryvtk.widgets.vtk_rendering_generator as rg
 
 
-def test_basic_rendering_generator(setup_vtk_err):
-
-    _, _ = setup_vtk_err
-
-    #Tutorial-section1
-    # [Rotation x,y,z Translation x,y,z]
-    model_to_world = [45, 45, 45, 0, 0, 0]
-    camera_to_world = [0, 0, 0, 47.5, 65, -300]
-    left_to_right = [0, 0, 0, 0, 0, 0]
-
-    model_file = "tests/data/rendering/models-calibration-pattern.json"
-    background_image = "tests/data/rendering/background-1920-x-1080.png"
-    cam_intrinsics = "tests/data/rendering/calib.left.intrinsic.txt"
-
-    #Tutorial-section2
-    generator = rg.VTKRenderingGenerator(model_file,
-                                         background_image,
-                                         cam_intrinsics,
-                                         camera_to_world,
-                                         left_to_right,
-                                         zbuffer=False
-                                         )
-
-    generator.set_all_model_to_world(model_to_world)
-    generator.set_clipping_range(200, 400)
-    generator.set_smoothing(2, 11)
-    generator.show()
-
-    #Tutorial-section3
-    img = generator.get_image()
-    bgr = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
-    cv2.imwrite("tests/output/rendering-m2w-1.png", bgr)
-    #Tutorial-section4
-    # Now check we get same image, if we use the other set_model_to_worlds.
-    generator.set_all_model_to_world([0, 0, 0, 0, 0, 0])  # to reset it.
-    dict_of_trans = {'calibration pattern': model_to_world}
-    generator.set_model_to_worlds(dict_of_trans)
-    generator.show()
-    img = generator.get_image()
-    bgr = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
-    cv2.imwrite("tests/output/rendering-m2w-2.png", bgr)
-
-    img_a = cv2.imread("tests/output/rendering-m2w-1.png")
-    img_b = cv2.imread("tests/output/rendering-m2w-2.png")
-    assert np.allclose(img_a, img_b)
-
-    # Now check we get ValueError if name is invalid.
-    dict_of_trans = {'banana': model_to_world}
-    with pytest.raises(ValueError):
-        generator.set_model_to_worlds(dict_of_trans)
-
-    # Now testing we can render z-buffer type images.
-    generator2 = rg.VTKRenderingGenerator("tests/data/rendering/models-calibration-pattern.json",
-                                          "tests/data/rendering/background-1920-x-1080.png",
-                                          "tests/data/rendering/calib.left.intrinsic.txt",
-                                          camera_to_world,
-                                          left_to_right,
-                                          zbuffer=True
-                                          )
-    generator2.set_all_model_to_world(model_to_world)
-    generator2.set_clipping_range(200, 400)
-    generator2.set_smoothing(0, 11)
-    generator2.show()
-
-    img = generator2.get_image()
-    cv2.imwrite("tests/output/rendering-zbuffer.png", img)
-
-    generator.close()
-    generator2.close()
+# def test_basic_rendering_generator(setup_vtk_err):
+#
+#     _, _ = setup_vtk_err
+#
+#     #Tutorial-section1
+#     # [Rotation x,y,z Translation x,y,z]
+#     model_to_world = [45, 45, 45, 0, 0, 0]
+#     camera_to_world = [0, 0, 0, 47.5, 65, -300]
+#     left_to_right = [0, 0, 0, 0, 0, 0]
+#
+#     model_file = "tests/data/rendering/models-calibration-pattern.json"
+#     background_image = "tests/data/rendering/background-1920-x-1080.png"
+#     cam_intrinsics = "tests/data/rendering/calib.left.intrinsic.txt"
+#
+#     #Tutorial-section2
+#     generator = rg.VTKRenderingGenerator(model_file,
+#                                          background_image,
+#                                          cam_intrinsics,
+#                                          camera_to_world,
+#                                          left_to_right,
+#                                          zbuffer=False
+#                                          )
+#
+#     generator.set_all_model_to_world(model_to_world)
+#     generator.set_clipping_range(200, 400)
+#     generator.set_smoothing(2, 11)
+#     generator.show()
+#
+#     #Tutorial-section3
+#     img = generator.get_image()
+#     bgr = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+#     cv2.imwrite("tests/output/rendering-m2w-1.png", bgr)
+#     #Tutorial-section4
+#     # Now check we get same image, if we use the other set_model_to_worlds.
+#     generator.set_all_model_to_world([0, 0, 0, 0, 0, 0])  # to reset it.
+#     dict_of_trans = {'calibration pattern': model_to_world}
+#     generator.set_model_to_worlds(dict_of_trans)
+#     generator.show()
+#     img = generator.get_image()
+#     bgr = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+#     cv2.imwrite("tests/output/rendering-m2w-2.png", bgr)
+#
+#     img_a = cv2.imread("tests/output/rendering-m2w-1.png")
+#     img_b = cv2.imread("tests/output/rendering-m2w-2.png")
+#     assert np.allclose(img_a, img_b)
+#
+#     # Now check we get ValueError if name is invalid.
+#     dict_of_trans = {'banana': model_to_world}
+#     with pytest.raises(ValueError):
+#         generator.set_model_to_worlds(dict_of_trans)
+#
+#     # Now testing we can render z-buffer type images.
+#     generator2 = rg.VTKRenderingGenerator("tests/data/rendering/models-calibration-pattern.json",
+#                                           "tests/data/rendering/background-1920-x-1080.png",
+#                                           "tests/data/rendering/calib.left.intrinsic.txt",
+#                                           camera_to_world,
+#                                           left_to_right,
+#                                           zbuffer=True
+#                                           )
+#     generator2.set_all_model_to_world(model_to_world)
+#     generator2.set_clipping_range(200, 400)
+#     generator2.set_smoothing(0, 11)
+#     generator2.show()
+#
+#     img = generator2.get_image()
+#     cv2.imwrite("tests/output/rendering-zbuffer.png", img)
+#
+#     generator.close()
+#     generator2.close()
 
 
 def test_mask_generator(setup_vtk_err):
