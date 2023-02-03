8bea57cd691852db7038f1d0ddde9cd9de2db73a
Miguel Xochicale <m.xochicale@ucl.ac.uk>
2023-02-03 12:52:10 +0000

    :heavy_check_mark: local [01-items] tests/text/test_large_centred_text.py (#203 #187)

--- tests/text/test_large_centred_text.py
+++ tests/text/test_large_centred_text.py
@@ -1,4 +1,4 @@
-import pytest
+# coding=utf-8
 
 from sksurgeryvtk.text.text_overlay import VTKLargeTextCentreOfScreen
 
@@ -14,6 +14,6 @@ def test_position_correct(setup_vtk_overlay_window):
     text_x, text_y = vtk_text.text_actor.GetPosition()
 
     window_width, window_height = vtk_overlay_window._RenderWindow.GetSize()
-    
+
     assert text_x == window_width / 2
     assert text_y == window_height / 2
