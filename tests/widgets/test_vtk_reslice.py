02ebed60f1671d55d5aadbdba32898b98469f8ff
Miguel Xochicale <m.xochicale@ucl.ac.uk>
2023-02-08 17:10:42 +0000

    :heavy_check_mark: 02-items locally passed tests/widgets/test_vtk_reslice.py (#203 #187)

--- tests/widgets/test_vtk_reslice.py
+++ tests/widgets/test_vtk_reslice.py
@@ -3,14 +3,14 @@
 from sksurgeryvtk.widgets import vtk_reslice_widget
 
 
-# def test_slice_viewer(qtbot):
-#     dicom_path = 'tests/data/dicom/LegoPhantom_10slices'
-#     reslice = vtk_reslice_widget.VTKSliceViewer(dicom_path)
-#
-#     qtbot.addWidget(reslice)
-#
-#     reslice.update_slice_positions_pixels(1, 1, 1)
-#
+def test_slice_viewer(qtbot):
+    dicom_path = 'tests/data/dicom/LegoPhantom_10slices'
+    reslice = vtk_reslice_widget.VTKSliceViewer(dicom_path)
+
+    qtbot.addWidget(reslice)
+
+    reslice.update_slice_positions_pixels(1, 1, 1)
+
 
 def test_mouse_scroll_slice_viewer(qtbot):
     dicom_path = 'tests/data/dicom/LegoPhantom_10slices'
