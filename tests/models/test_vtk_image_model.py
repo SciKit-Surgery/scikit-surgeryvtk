d0f72a4fa912af7685fe83731bfe923f31fb6b8a
Miguel Xochicale <m.xochicale@ucl.ac.uk>
2023-02-08 12:52:41 +0000

    :heavy_check_mark: passed 02-items local tests/models/test_vtk_image_model.py  (#203 #187)

--- tests/models/test_vtk_image_model.py
+++ tests/models/test_vtk_image_model.py
@@ -9,14 +9,14 @@ def test_valid_image_model_matt():
     assert model.actor is not None
 
 
-# def test_image_model_overlay(vtk_overlay_with_gradient_image):
-#
-#     image, widget, _, app = vtk_overlay_with_gradient_image
-#     input_file = 'tests/data/calibration/left-1095-undistorted.png'
-#     model = [VTKImageModel(input_file)]
-#     widget.add_vtk_models(model)
-#     widget.resize(1920, 1080)
-#     widget.show()
-#     widget.Render()
-#     #app.exec_()
+def test_image_model_overlay(vtk_overlay_with_gradient_image):
+
+    image, widget, _, app = vtk_overlay_with_gradient_image
+    input_file = 'tests/data/calibration/left-1095-undistorted.png'
+    model = [VTKImageModel(input_file)]
+    widget.add_vtk_models(model)
+    widget.resize(1920, 1080)
+    widget.show()
+    widget.Render()
+    #app.exec_()
 
