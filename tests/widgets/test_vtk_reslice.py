cdb1dffecc61c3b15d0438025948e20ed10af313
Miguel Xochicale <m.xochicale@ucl.ac.uk>
2023-02-08 16:36:29 +0000

    :x: tests/widgets/test_vtk_reslice.py::test_slice_viewer (#203 #187)

--- tests/widgets/test_vtk_reslice.py
+++ tests/widgets/test_vtk_reslice.py
@@ -3,15 +3,15 @@
 from sksurgeryvtk.widgets import vtk_reslice_widget
 
 
-# def test_slice_viewer(qtbot):
-#     dicom_path = 'tests/data/dicom/LegoPhantom_10slices'
-#     reslice = vtk_reslice_widget.VTKSliceViewer(dicom_path)
-#
-#     qtbot.addWidget(reslice)
-#
-#     reslice.update_slice_positions_pixels(1, 1, 1)
+def test_slice_viewer(qtbot):
+    dicom_path = 'tests/data/dicom/LegoPhantom_10slices'
+    reslice = vtk_reslice_widget.VTKSliceViewer(dicom_path)
 
+    qtbot.addWidget(reslice)
 
+    reslice.update_slice_positions_pixels(1, 1, 1)
+
+#
 # def test_mouse_scroll_slice_viewer(qtbot):
 #     dicom_path = 'tests/data/dicom/LegoPhantom_10slices'
 #     reslice = vtk_reslice_widget.MouseWheelSliceViewer(dicom_path)
