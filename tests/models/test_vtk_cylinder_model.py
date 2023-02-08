6199da4cb07c59e8d661938627b5e92bbdadfa2d
Miguel Xochicale <m.xochicale@ucl.ac.uk>
2023-02-08 12:27:57 +0000

    :heavy_check_mark: passed 02-items local tests/models/test_vtk_cylinder_model.py  (#203 #187)

--- tests/models/test_vtk_cylinder_model.py
+++ tests/models/test_vtk_cylinder_model.py
@@ -19,24 +19,24 @@ def test_cylinder_model_invalid():
         cm.VTKCylinderModel(None, 0.5)
 
 
-# def test_cylinder_model(setup_vtk_overlay_window):
-#     """
-#     Tests that a widget can access the actor created.
-#     """
-#     height = 50.0
-#     radius = 10.0
-#     colour = (0.0, 1.0, 0.0)
-#     name = "test Cylinder"
-#     angle = 45.0
-#     orientation = (1.0, 0.0, 0.0)
-#     resolution = 45
-#     visibility = True
-#     opacity = 0.5
-#
-#     vtk_model = cm.VTKCylinderModel(height, radius, colour, name, angle,
-#                                     orientation, resolution, visibility,
-#                                     opacity)
-#
-#     widget, _, _ = setup_vtk_overlay_window
-#     widget.add_vtk_actor(vtk_model.actor)
-#     widget.show()
+def test_cylinder_model(setup_vtk_overlay_window):
+    """
+    Tests that a widget can access the actor created.
+    """
+    height = 50.0
+    radius = 10.0
+    colour = (0.0, 1.0, 0.0)
+    name = "test Cylinder"
+    angle = 45.0
+    orientation = (1.0, 0.0, 0.0)
+    resolution = 45
+    visibility = True
+    opacity = 0.5
+
+    vtk_model = cm.VTKCylinderModel(height, radius, colour, name, angle,
+                                    orientation, resolution, visibility,
+                                    opacity)
+
+    widget, _, _ = setup_vtk_overlay_window
+    widget.add_vtk_actor(vtk_model.actor)
+    widget.show()
