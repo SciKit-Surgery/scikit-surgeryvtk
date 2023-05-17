4035931c258ba5f79dc3df23da35166096262ea5
Miguel Xochicale <m.xochicale@ucl.ac.uk>
2023-05-17 18:16:06 +0100

    removes dep nbsphinx to build docs (#216, #217)

--- docs/conf.py
+++ docs/conf.py
@@ -84,7 +84,7 @@ def setup(app):
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode', 'sphinx.ext.imgmath', 'nbsphinx']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode', 'sphinx.ext.imgmath']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
